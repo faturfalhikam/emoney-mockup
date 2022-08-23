e43191c48cdb0f7f88e173430a6d16643dcffe6e
Ageng D. Prastyawan <rexcarnation@gmail.com>
2022-08-23 15:36:21 +0700

    add sku

--- config/product.php
+++ config/product.php
@@ -6,39 +6,37 @@ return [
         'price'         => 3000,
         'description'   => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta lectus ut enim mollis blandit. Quisque posuere nibh vitae dictum eleifend. Integer justo magna, molestie vitae volutpat quis, convallis sit amet ex. Fusce molestie neque in suscipit pellentesque. Nam molestie enim eget nisi efficitur, in ultricies orci interdum. Praesent quam diam, congue non consectetur non, congue nec tellus. Vivamus non hendrerit risus. Mauris ac dapibus lacus, non ultricies magna. In iaculis nibh odio, nec ultrices lectus facilisis et. Integer consequat diam eu magna commodo, consectetur placerat lacus tristique. Morbi mollis sem ac vehicula aliquam. Suspendisse tristique dapibus arcu, sit amet rhoncus quam dignissim quis. Vivamus feugiat elit vitae ante commodo fringilla. Etiam ac nulla sollicitudin, imperdiet urna sed, facilisis eros.'
     ],
-    'gelas'    => [
+    'sku-ab76jf'    => [
         'name'          => 'Gelas Kaca',
         'price'         => 4000,
         'description'   => 'Quisque egestas turpis ut lorem eleifend, et malesuada arcu interdum. Vivamus gravida efficitur tellus at egestas. Nam et massa eu sem pellentesque tempor. Integer et eros in arcu sagittis pretium. Donec sodales varius tempus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae fringilla nisi. Morbi tristique purus tellus. Praesent pellentesque molestie tellus, sit amet tincidunt orci luctus ac. Suspendisse potenti. Fusce pulvinar nunc tellus, luctus vulputate nunc posuere ut. Phasellus in eros lorem. Nunc nec justo quis est vulputate porta sed vel sem.'
     ],
-    'tumblr'    => [
+    'sku-ab76jg'    => [
         'name'          => 'Tumblr',
         'price'         => 5000,
         'description'   => 'Sed ac lacinia nulla. Morbi ultricies cursus enim eu tincidunt. Duis nunc ipsum, fermentum id elit convallis, tempor volutpat sem. Nullam rhoncus pharetra sapien id tristique. Sed et quam mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce pharetra finibus nulla eu congue. Donec eget elit arcu.'
     ],
-    'sendok'    => [
+    'sku-ab76jh'    => [
         'name'          => 'Sendok Set',
         'price'         => 6000,
         'description'   => 'Praesent sit amet lorem aliquet, congue ante a, volutpat magna. Maecenas cursus malesuada quam, cursus consequat tortor ultricies quis. Maecenas sem nisl, ornare a vulputate vitae, egestas a nibh. Vestibulum est quam, finibus vitae vulputate ut, pretium eu arcu. Donec faucibus elementum tincidunt. Morbi at ornare arcu. Quisque quis nibh non odio faucibus tempor a non turpis. Maecenas egestas lacus nisi, ac tristique nunc viverra sit amet. Sed eu consequat est. Etiam vitae lacinia odio, non ultrices risus. Cras semper ex ut ipsum mattis faucibus. Vivamus tempor dolor lorem, eget commodo elit viverra eget. Etiam eleifend ante gravida, consequat augue sed, consequat dolor.'
     ],
-
-    'wajan'    => [
+    'sku-ab76ji'    => [
         'name'          => 'Wajan Kecil',
         'price'         => 12000,
         'description'   => 'Aliquam purus enim, gravida sit amet accumsan sed, elementum et sem. Pellentesque in porttitor leo. Fusce urna dui, condimentum eu feugiat vitae, rhoncus et dolor. Praesent auctor metus euismod magna pellentesque volutpat. Duis et convallis sem, sed euismod arcu. Nunc sit amet leo a lacus tempus elementum vel sit amet sapien. Aliquam erat volutpat. Sed augue turpis, aliquam at cursus non, interdum non diam.'
     ],
-    'wajan2'    => [
+    'sku-ab76jj'    => [
         'name'          => 'Wajan Besar',
         'price'         => 12000,
         'description'   => 'Aliquam purus enim, gravida sit amet accumsan sed, elementum et sem. Pellentesque in porttitor leo. Fusce urna dui, condimentum eu feugiat vitae, rhoncus et dolor. Praesent auctor metus euismod magna pellentesque volutpat. Duis et convallis sem, sed euismod arcu. Nunc sit amet leo a lacus tempus elementum vel sit amet sapien. Aliquam erat volutpat. Sed augue turpis, aliquam at cursus non, interdum non diam.'
     ],
-    'kompor'    => [
+    'sku-ab76jk'    => [
         'name'          => 'Coffee Beans',
         'price'         => 35000,
         'description'   => 'Aliquam purus enim, gravida sit amet accumsan sed, elementum et sem. Pellentesque in porttitor leo. Fusce urna dui, condimentum eu feugiat vitae, rhoncus et dolor. Praesent auctor metus euismod magna pellentesque volutpat. Duis et convallis sem, sed euismod arcu. Nunc sit amet leo a lacus tempus elementum vel sit amet sapien. Aliquam erat volutpat. Sed augue turpis, aliquam at cursus non, interdum non diam.'
     ],
-
-    'magic'    => [
+    'sku-ab76jl'    => [
         'name'          => 'Magic Jar',
         'price'         => 335000,
         'description'   => 'Aliquam purus enim, gravida sit amet accumsan sed, elementum et sem. Pellentesque in porttitor leo. Fusce urna dui, condimentum eu feugiat vitae, rhoncus et dolor. Praesent auctor metus euismod magna pellentesque volutpat. Duis et convallis sem, sed euismod arcu. Nunc sit amet leo a lacus tempus elementum vel sit amet sapien. Aliquam erat volutpat. Sed augue turpis, aliquam at cursus non, interdum non diam.'
